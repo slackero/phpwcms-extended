f5e86f9cad7e94dd52e28c2570ac98d92d90d8d5
Oliver Georgi <slackero+github@gmail.com>
2013-07-30 11:51:09 +0200

    Implement options

--- phpwcms-cycle-slider/template/lib/cycle-slider/init-slider.js
+++ phpwcms-cycle-slider/template/lib/cycle-slider/init-slider.js
@@ -6,49 +6,67 @@
  *
  */
 $(function(){
-
+	
+	var $sliderOptions = {
+		autoSizeImage: true, // if false the Slider Item will be used as is
+		enablePrevNext: true,
+		enablePagination: true,
+		
+		// jQuery Cycle plugin options
+		// http://jquery.malsup.com/cycle/options.html
+		cycleOptions: {
+			fx: 'scrollRight',
+			speed: 'slow',
+			slideExpr: '.slider-item',
+			pauseOnPagerHover: 1,
+			pause: 1,
+			timeout: 5000,
+			startingSlide: 0,
+			fit: 1,
+			slideResize: 0
+		}
+	};
+	
 	var $sliderSection = $('.cycle-slider-section');
+	
 	if($sliderSection) {
 
 		var $sliderItems = $('.slider-item', $sliderSection);
 		
-		// Select first <img> Tag, catch image src and use it as CSS background image
-		// Then hide the <img> Tag
-		$sliderItems.each(function() {
-			var $item = $(this);
-			var $itemImage = $('img:first', $item);
-			var $itemImageSrc = $itemImage.attr('src');
-			if($itemImageSrc) {
-				$item.css('background-image', 'url(' + $itemImageSrc + ')');
-				$itemImage.hide();
-			}
-		});
+		if($sliderOptions.autoSizeImage) {
+			
+			// Select first <img> Tag, catch image src and use it as CSS background image
+			// Then hide the <img> Tag
+			
+			$sliderItems.each(function() {
+				var $item = $(this);
+				var $itemImage = $('img:first', $item);
+				var $itemImageSrc = $itemImage.attr('src');
+				if($itemImageSrc) {
+					$item.css('background-image', 'url(' + $itemImageSrc + ')');
+					$itemImage.hide();
+				}
+			});
+		}
 
 		// Cycle but only when more than 1 item
 		if($sliderItems.length > 1) {
 			
 			// Add Prev/Next and Pagination (Dots)
-			$sliderSection
-				.append('<a id="slider-item-next" href="#" class="icon-chevron-sign-right icon-3x slider-nav">')
-				.append('<a id="slider-item-prev" href="#" class="icon-chevron-sign-left icon-3x slider-nav">')
-				.append('<div id="slider-pagination" class="slider-pagination">');
+			if($sliderOptions.enablePrevNext) {
+				$sliderSection
+					.append('<a id="slider-item-next" href="#" class="icon-chevron-sign-right icon-3x slider-nav">')
+					.append('<a id="slider-item-prev" href="#" class="icon-chevron-sign-left icon-3x slider-nav">');
+				
+				$sliderOptions.cycleOptions.next = '#slider-item-next';
+				$sliderOptions.cycleOptions.prev = '#slider-item-prev';
+			}
+			if($sliderOptions.enablePagination) {
+				$sliderSection.append('<div id="slider-pagination" class="slider-pagination">');
+				$sliderOptions.cycleOptions.pager = '#slider-pagination';
+			}
 			
-			// jQuery Cycle plugin options
-			// http://jquery.malsup.com/cycle/options.html
-			$sliderSection.cycle({
-				fx: 'scrollRight',
-				speed: 'slow',
-				pager: '#slider-pagination',
-				next: '#slider-item-next',
-				prev: '#slider-item-prev',
-				slideExpr: '.slider-item',
-				pauseOnPagerHover: 1,
-				pause: 1,
-				timeout: 5000,
-				startingSlide: 0,
-				fit: 1,
-				slideResize: 0
-			});
+			$sliderSection.cycle($sliderOptions.cycleOptions);
 		}
 	}
 });
